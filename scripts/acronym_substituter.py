e9f06f98fc247bdc3b1ac494d39d2ee9f728dab8
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-02-12 14:55:23 +0200

    Added scripts to the repo

---
+++ scripts/acronym_substituter.py
@@ -0,0 +1,134 @@
+import re
+import shutil
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dry", action="store_true")
+parser.add_argument("files", nargs="+")
+
+args = parser.parse_args()
+
+def colourize(text, colour_code="154"):
+    return f"\033[38;5;{colour_code}m{text}\033[0m"
+
+def load_acronyms(file):
+    acronym_pattern = re.compile(r"\\newacronym\{(?P<label>.+)\}\{(?P<acronym>.+)\}\{(?P<description>.+)\}")
+    acronyms = {}
+    
+    with open(file) as f:
+        for line in f:
+            m = acronym_pattern.match(line)
+            if m:
+                acronyms[m.group("acronym")] = { "label": m.group("label"), "description": m.group("description") }
+
+    return acronyms
+
+
+acronyms = load_acronyms("content/acronyms.tex")
+
+def generate_acrfull_patterns():
+    acrfull_patterns = []
+    
+    for acronym, data in acronyms.items():
+        description = data["description"]
+        pattern = r"(?P<description>%s)(?P<plural>s?)\s+\((?P<acronym>%s)\2\)" % (description, acronym)
+        acrfull_patterns.append(re.compile(pattern, flags=re.IGNORECASE))
+    
+    return acrfull_patterns
+
+
+acrfull_patterns = generate_acrfull_patterns()
+
+def acrfull_sub(m):
+    command = "\\acrfull"
+
+    matched = m.group("description")
+    original = acronyms[m.group("acronym")]["description"]
+    
+    if matched == original.upper():
+        command = "\\ACRfull"
+    
+    elif matched[0] == original[0].upper():
+        command = "\\Acrfull"
+
+    if m.group("plural"):
+        command += "pl"
+    
+    label = acronyms[m.group("acronym")]["label"]
+    command += f"{{{label}}}"
+
+    if args.dry:
+        command = colourize(command, 154)
+    return command
+
+def acrshort_sub(m):
+    acronym = m.group('acronym')
+    
+    if acronym not in acronyms:
+        if args.dry:
+            acronym = colourize(acronym, 196)
+
+        return acronym
+
+    command = "\\acrshort{%s}" % acronyms[acronym]["label"]
+    
+    if args.dry:
+        command = colourize(command, 154)
+
+    return command
+
+acrshort_pattern = re.compile(r"\b(?P<acronym>[A-Z][A-Z0-9\-]+)\b")
+ignored_patterns = [
+    # comments
+    re.compile("^%"),
+
+    # section commands
+    re.compile(r"^\\(sub){,2}(section|chapter|paragraph)"),
+    
+    # captions
+    re.compile(r"^\\caption"),
+]
+
+for file in args.files:
+    open_environments = 0
+    new_lines = []
+    made_changes = False
+    
+    if not args.dry:
+        shutil.copy(file, file + ".bak")
+    
+    with open(file) as f:
+        for line in f:
+            if line.startswith("\\begin"):
+                open_environments += 1
+
+            elif line.startswith("\\end"):
+                open_environments -= 1
+
+            new_line = line
+            
+            inside_environment = open_environments > 0
+            any_ignored = any(map(lambda p: p.match(line), ignored_patterns))
+                        
+            if not (any_ignored and inside_environment):
+                for pat in acrfull_patterns:
+                    if pat.search(new_line):
+                        new_line = pat.sub(acrfull_sub, new_line)
+
+                new_line = acrshort_pattern.sub(acrshort_sub, new_line)
+                
+                if not made_changes and line != new_line:
+                    print(f"made changes '{line}' != '{new_line}'")
+                    made_changes = True
+            
+            new_lines.append(new_line)
+    
+    if made_changes:
+        if args.dry:
+            for current_line in new_lines:
+                print(current_line.strip())
+        else:
+            with open(file, "w") as f:
+                f.writelines(new_lines)
+    else:
+        print(colourize(f">> NO CHANGES MADE in `{file}` >>", 214))
---
+++ scripts/split_lines.py
@@ -0,0 +1,54 @@
+import re
+import shutil
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dry", action="store_true")
+parser.add_argument("files", nargs="+")
+
+args = parser.parse_args()
+
+paragraph = re.compile(r"[A-Z].+$")
+# paragraph = re.compile(r"^$")
+
+for file in args.files:
+    new_lines = []
+    
+    if not args.dry:
+        shutil.copy(file, file + ".bak")
+    
+    with open(file) as f:
+        lines = f.readlines()
+        total_lines = len(lines) - 1
+        lnum = 0
+        open_environments = 0
+        made_changes = False
+        
+        for lnum in range(total_lines):
+            current_line = lines[lnum]
+            next_line = lines[lnum + 1]
+            
+            if current_line.startswith("\\begin"):
+                open_environments += 1
+                
+            elif current_line.startswith("\\end"):
+                open_environments -= 1
+                
+            if open_environments == 0 and paragraph.match(current_line) and next_line.strip():
+                current_line += "\033[38;5;154m>> NEWLINE HERE >>\033[0m" if args.dry else "\n"
+                made_changes = True
+
+            new_lines.append(current_line)
+        
+        # add the final line of the file
+        new_lines.append(lines[lnum])
+    
+    if made_changes:
+        if args.dry:
+            for current_line in new_lines:
+                print(current_line.strip())
+        else:
+            with open(file, "w") as f:
+                f.writelines(new_lines)
+    else:
+        print("\033[38;5;214m>> NO CHANGES MADE >>\033[0m")
